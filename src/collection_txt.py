06bef6bb5a2bf3ffe5c6d5e7a65015a45f87f8fa
jimho at myfreeyun <box8685@tom.com>
2019-12-01 18:36:23 +0800

    uodate to create wantting format cvs

--- src/collection_txt.py
+++ src/collection_txt.py
@@ -6,6 +6,8 @@ import re
 from bs4 import BeautifulSoup
 import os
 import configparser
+import numpy as np
+import pandas as pd
 
 # 读取config.cfg文件得到动态的网址和目标名称
 def read_url_aimname():
@@ -13,7 +15,7 @@ def read_url_aimname():
     if os.path.exists(os.path.join(os.getcwd(), config_file_url)):
         config = configparser.ConfigParser()
         config.read(config_file_url)
-        data = config.get('Text_Feature', 'feature1_3')
+        data = config.get('Text_Feature', 'feature1_1')
         #print(data)
         #data_list = re.split(r',',data)
         one_url = eval(data)
@@ -22,7 +24,7 @@ def read_url_aimname():
 # 访问网站取得数据
 def get_value(one_url):
     find_condition = []
-    for i in range(len(one_url)):
+    for i in np.arange(len(one_url)):
         if i == 1 :
             # 判断http 或 https
             url = one_url[i]
@@ -41,7 +43,7 @@ def get_value(one_url):
         if i > 1 :
             find_condition.append(one_url[i])
     # 提取有用信息
-    print(find_condition)
+    #print(find_condition)
     return take_useful_message(page, find_condition)           
     
 
@@ -77,10 +79,37 @@ def take_useful_message(html_origin, condition):
         data = message.get_text().split()
     print(u'data len are : {}'.format(len(data)))
     return data
-    
 
+# 按日期，内容入成文件
+def write_to_file(data_list, file_name):
+    # 找日期特征的内容
+    find_date_obj =  re.compile(r'\d{2,4}\W*\d{0,2}\W*\d{0,2}',flags = 0)
+    date_list = []  
+    text_list = []
+    text_temp = ''
+    for text in data_list:
+        rel = find_date_obj.match(text)
+        if rel:
+            date_list.append(rel.group())
+            text_list.append(text_temp)
+            text_temp = ''
+        else:
+            text_temp = text_temp + text
+    #print('text : {}'.format(text_list))
+    #print('date : {}'.format(date_list))
+    rel = make_pandas(date_list, text_list, mode = 'create')
+    print(rel)
+    rel.to_csv(file_name, encoding = 'utf-8')
+    return True
+
+def make_pandas(date_list, text_list, mode):    
+    if len(date_list) == len(text_list):
+        if mode == 'create':
+            df = pd.DataFrame(data = {'date':date_list, 'text':text_list}, index = np.arange(len(date_list)))
+    return df
+        
 if __name__=='__main__':
     b= read_url_aimname()
     print(b)
-    print(get_value(b))
+    write_to_file(get_value(b),'./store_text.csv')
     
