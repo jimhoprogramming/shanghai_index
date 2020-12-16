a89da633aa673c3f868ca78e48a0dc2c54a14a9e
jimho <Box8685@Tom.com>
2020-12-16 14:51:00 +0800

    ready over creat data for x y one part

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -9,6 +9,7 @@ from mxnet import nd, gluon, init, cpu
 from mxnet.gluon import data as gdata, loss as gloss 
 import re
 import d2l
+import tushare as ts
 
 
 ##pre_trained_vector_files_url = 'c://w2v.txt''
@@ -18,10 +19,17 @@ db_url = '//home//jim//shanghai_index//data//all.db'
 ##train_data_url = 'd://github_project//shanghai_index//data//simplifyweibo_4_moods.csv'
 ##train_data_url = '//home//jim//shanghai_index//data//simplifyweibo_4_moods.csv'
 ##train_data_url = 'd://github_project//shanghai_index//src//train_set.csv'
-train_data_url = '//home//jim//shanghai_index//src//train_set.csv'
+train_data_url_txt = '//home//jim//shanghai_index//data//store_text.csv'
+train_data_url_dig = '//home//jim//shanghai_index//data//store_digital.csv'
 
-# 提取中文向量数据矩阵: 所有关键词提取300d向量到内存变量暂存
+
+
+
+
 def load_vec_to_memory():
+    '''
+        提取中文向量数据矩阵: 所有关键词提取300d向量到内存变量暂存
+    '''
     embeddings_index = {}
     f = open(pre_trained_vector_files_url, 'r', encoding = 'utf-8', error = 'ignore')
     i = 0
@@ -34,8 +42,10 @@ def load_vec_to_memory():
     f.close()
     return embeddings_index
 
-# 文本数据导入成数据库文件
 def text_to_sql():
+    '''
+        文本数据导入成数据库文件
+    '''
     sql_db = sqlite3.connect(db_url)
     cur = sql_db.cursor()
     cur.execute('create table if not exists w2v(id integer primary key, word text, vectors text)')
@@ -55,8 +65,10 @@ def text_to_sql():
     f.close()
     return True
 
-# 建立词向量表的索引加速查询速度 
 def create_word_index():
+    '''
+        建立词向量表的索引加速查询速度 
+    '''
     sql_db = sqlite3.connect(db_url)
     cur = sql_db.cursor()
     cur.execute('create index if not exists word_idx on w2v(word)')
@@ -64,8 +76,10 @@ def create_word_index():
     cur.close()
     return True
 
-# 在数据库查指定词语的向量
 def one_word_to_vector(words):
+    '''
+        在数据库查指定词语的向量
+    '''
     sql_db = sqlite3.connect(db_url)
     cur = sql_db.cursor()
     result = []
@@ -86,25 +100,33 @@ def one_word_to_vector(words):
     sql_db.close()
     return result
 
-# 去除sql3不接受的字符
 def drop_char(word):
+    '''
+        去除sql3不接受的字符
+    '''
     a = re.findall(r'\w*', word, re.S) 
     return a[0]
 
 
-# 实现词语转换成向量
 def one_word_to_vec(dest_word, embeddings_index):
+    '''
+        实现词语转换成向量
+    '''
     return embeddings_index[dest_word]
 
-# 把一句话以词的形式分开成一列表
 def sentence_to_word_list(sentence):
+    '''
+        把一句话以词的形式分开成一列表
+    '''
     seg_list = jieba.lcut_for_search(sentence)
     seg_list = [drop_char(word = w) for w in seg_list]
     #print(seg_list)
     return seg_list
 
-# 读取数据文件，返回文本数据x，y
 def read_text_data(url):
+    '''
+        读取数据文件，返回文本数据x，y
+    '''
     data = pd.read_csv(url, encoding = 'utf-8', index_col = 0)
     data.reindex([i for i in range(data.shape[0])], fill_value = '4')
     #x = x['text'].tolist()
@@ -114,8 +136,10 @@ def read_text_data(url):
     return x,y
 
 
-# 截断或补全处理一句话
 def preprocess_imdb(x, y):  # 本函数已保存在d2lzh包中方便以后使用
+    '''
+    # 截断或补全处理一句话
+    '''
     max_l = 100  # 将每条评论通过截断或者补'<pad>'，使得长度变成500
     def pad(x):
         return x[:max_l] if len(x) > max_l else x + [u'空格'] * (max_l - len(x))
@@ -125,20 +149,36 @@ def preprocess_imdb(x, y):  # 本函数已保存在d2lzh包中方便以后使用
     labels = nd.array([y])
     return features, labels
 
+def define_model():
+    '''
+        载入模型
+    '''
+    model = BiRNN(vocab = 100, embed_size = 300, num_hiddens = 200, num_layers = 2)
+    lr, num_epochs = 0.001, 125
+    model.initialize(init.Xavier(), ctx=cpu())
+    trainer = gluon.Trainer(model.collect_params(), 'adam', {'learning_rate':lr})
+    loss = gloss.SoftmaxCrossEntropyLoss(sparse_label = True)
+    return model, trainer, loss
 
-# 建立迭代器
-def create_iter():
-    # 为缓解速度问题打散数据在先，提取词向量在后
-    batch_size = 32
-    s_t_d_obj = short_time_dataset(train_data_url)
-    train_iter = gdata.DataLoader(s_t_d_obj, batch_size, shuffle = True)
-    return train_iter
+# 初始化参数
 
-# 自建一个按次提取的数据集
+
+def run_train(model, data, trainer, loss, num_epochs, ctx):
+    '''
+        实施训练
+    '''
+    test_iter = data
+    d2l.train(data, test_iter, model, loss, trainer, ctx, num_epochs)
+    return model
+    #
+
 class short_time_dataset(gdata.Dataset):
+    '''
+        自建一个按次提取的数据集
+    '''
     def __init__(self, url):
         super(short_time_dataset, self).__init__()
-        self.data = pd.read_csv(url, encoding = 'utf-8',index_col = 0)
+        self.data = self.__open_file(url = url)
         self.data = self.data.reindex([i for i in range(self.data.shape[0])], fill_value = '4')
     def __len__(self):
         print('data numbers = {}'.format(self.data.shape[0]))
@@ -156,25 +196,78 @@ class short_time_dataset(gdata.Dataset):
         #y = nd.one_hot(y,5)
         #y = nd.squeeze(y)
         return x, y
-        
 
-# 载入模型
-def define_model():
-    model = BiRNN(vocab = 100, embed_size = 300, num_hiddens = 200, num_layers = 2)
-    lr, num_epochs = 0.001, 125
-    model.initialize(init.Xavier(), ctx=cpu())
-    trainer = gluon.Trainer(model.collect_params(), 'adam', {'learning_rate':lr})
-    loss = gloss.SoftmaxCrossEntropyLoss(sparse_label = True)
-    return model, trainer, loss
+def open_file(url):
+    '''
+        提取文件；按日期合并text的行；按日期计算下一天的y；
+    '''
+    try :
+        data_text = pd.read_csv(url[0], encoding = 'utf-8',index_col = 0)
+    except:
+        try:
+            data_text = pd.read_csv(url[0], encoding = 'gbk',index_col = 0)
+        except:
+            data_text = pd.read_csv(url[0], encoding = 'gb2312',index_col = 0)
+    data_digital = pd.read_csv(url[1], encoding = 'utf-8',index_col = 0)
+    
+    print(data_text.shape)
+    data_text.drop_duplicates(['text'], inplace = True)
+    print(data_text.shape)
+    grouped = data_text.groupby(['date'])
+    data_text_dict = {}
+    for name, group in grouped:
+        temp_data = ';'.join(group['text'].tolist())
+        data_text_dict[name] = temp_data
+    # 按日期先后排序好数据计算所有需要的y值
+    data_digital = data_digital.set_index(['str_date'])
+    data_digital = data_digital.sort_index(ascending = True)
+    y_next = (data_digital['index_low'] - data_digital['index_before']) / data_digital['index_before']
+    y_next = y_next.tolist()[1:]
+    y_next.append(0)
+    #print(y_next)
+    data_digital['y_value'] = y_next
+    #print(data_digital)
+    # 生成有效日期列表，清除周六日数据或非交易日数据行
+    date_list = []
+    for date in data_digital.index:
+        if CheckTradeDateTrue(date):
+            date_list.append(date)
+    return date_list, data_text_dict, data_digital
 
-# 初始化参数
+def CheckTradeDateTrue(InputDate=None):
+    '''
+        功能：检查输入的日期是否交易日，
+        输入：'2017-10-11'格式日期
+        输出：是或否
+    '''
+    Rel = True
+    if InputDate is not None:
+        TempDate=InputDate
+        print(u'被判断的日期是:'+TempDate)
+        #检查那天上证指数是否空            
+        KData=ts.get_k_data('000001',start=TempDate,end=TempDate)
+        if KData is None:
+            Rel = False
+        elif len(KData)<1:
+            Rel = False
+    if Rel:
+        print(u'是交易日')
+    else:
+        print(u'不是交易日')
+    return Rel
+    
 
-# 实施训练
-def run_train(model, data, trainer, loss, num_epochs, ctx):
-    test_iter = data
-    d2l.train(data, test_iter, model, loss, trainer, ctx, num_epochs)
-    return model
-    #
+def create_iter():
+    '''
+        建立迭代器
+        为缓解速度问题打散数据在先，提取词向量在后
+    '''
+    batch_size = 32
+    s_t_d_obj = short_time_dataset([train_data_url_txt, train_data_url_dig])
+    train_iter = gdata.DataLoader(s_t_d_obj, batch_size, shuffle = True)
+    return train_iter
+
+
 
 
 
@@ -211,6 +304,10 @@ if __name__ == '__main__':
 ##    '#batches:', len(iteror)
 
     # test model
-    iteror = create_iter()
-    model, trainer, loss = define_model()
-    run_train(model = model, data = iteror, trainer = trainer, loss = loss, num_epochs = 1000, ctx = cpu())
+##    iteror = create_iter()
+##    model, trainer, loss = define_model()
+##    run_train(model = model, data = iteror, trainer = trainer, loss = loss, num_epochs = 1000, ctx = cpu())
+
+    # test open_file
+    date_list, date_t, data_d = open_file([train_data_url_txt, train_data_url_dig])
+    print(date_list)
